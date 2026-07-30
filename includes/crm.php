15e5bf66746a9f8b28b5a6a0efb23194c9952086
Colleen Diamond <cdiamond@winslowtg.com>
2026-07-29 21:29:59 -0400

    Mapping account data; getting choice metadata

--- includes/crm.php
+++ includes/crm.php
@@ -46,14 +46,49 @@ function getAccessToken(): ?string {
     return is_array($data) && array_key_exists('access_token', $data) ? $data['access_token'] : null;
 }
 
+// get choice metadata --> maps integers to labels for option set columns
+function getChoiceOptions(string $table, string $column, ?string $accessToken): ?array {
+    
+    $environmentUrl = getEnvironmentUrl();
+    if (!$environmentUrl) return null;
+
+    $url = $environmentUrl . "/api/data/v9.2/EntityDefinitions(LogicalName='{$table}')" .
+        "/Attributes(LogicalName='{$column}')/Microsoft.Dynamics.CRM.PicklistAttributeMetadata" .
+        '?$select=LogicalName&$expand=OptionSet,GlobalOptionSet&LabelLanguages=1033';
+    $options = [CURLOPT_HTTPGET => true];
+    $headers = [
+        'Authorization: Bearer ' . $accessToken,
+        'Accept: application/json',
+        'OData-MaxVersion: 4.0',
+        'OData-Version: 4.0',
+    ];
+    $data = sendRequest($url, $options, $headers);
+
+    if (!is_array($data)) return null;
+    $optionSet = $data['GlobalOptionSet'] ?? $data['OptionSet'] ?? null;
+    if (!is_array($optionSet) || !isset($optionSet['Options'])) return null;
+
+    $mapping = [];
+    foreach ($optionSet['Options'] as $option) {
+        if (!isset($option['Value'])) continue;
+        $label = $option['Label']['UserLocalizedLabel']['Label'] ?? null;
+        if ($label === null) continue;
+        $mapping[(int) $option['Value']] = $label;
+    }
+
+    return $mapping;
+}
+
 // get account data from crm
 function getAccountData(string $account, ?string $accessToken): ?array {
 
     $environmentUrl = getEnvironmentUrl();
-    if (!$environmentUrl) null;
+    if (!$environmentUrl) return null;
 
     $query = http_build_query([
-        '$select' => 'accountid,name,revenue,numberofemployees',
+        '$select' => 'accountid,name,websiteurl,wtg_industry,numberofemployees,revenue,'
+            . 'address1_city,address1_stateorprovince,address1_country,'
+            . 'customertypecode,description',
         '$filter' => "name eq '{$account}'",
         '$top' => 1,
     ], '', '&', PHP_QUERY_RFC3986);
@@ -67,10 +102,29 @@ function getAccountData(string $account, ?string $accessToken): ?array {
     ];
     $data = sendRequest($url, $options, $header);
 
-    return is_array($data) && array_key_exists('value', $data) ? $data['value'] : null;
+    return is_array($data) && array_key_exists('value', $data) ? $data['value'][0] : null;
 }
 
 // map account data to profile questions
+function mapAccountData(array $data): ?array {
+    if ($data === null) return null;
+    return [
+        'website' => $data['websiteurl'] ?? '',
+        'industry' => $data['wtg_industry'] ?? '',
+        'employee_count' => $data['numberofemployees'] ?? '',
+        'annual_revenue' => $data['revenue'] ?? '',
+        'location' => [
+            'city' => $data['address1_city'] ?? '',
+            'state' => $data['address1_stateorprovince'] ?? '',
+            'country' => $data['address1_country'] ?? '',
+        ],
+        'relationship' => $data['customertypecode'] ?? '',
+        'description' => $data['description'] ?? '',
+    ];
+}
 
 $token = getAccessToken();
-var_dump(getAccountData('10Pearls', $token));
+$data = getAccountData('Advantage Surveillance', $token);
+$map = mapAccountData($data);
+var_dump($map);
+var_dump(getChoiceOptions('account', 'customertypecode', $token));
